ae83d6eafdcb71b50c9e024474fbf1fd40c7d0f9
sumit saini <sumitsaini95488@gamil.com>
2023-07-24 14:34:09 +0530

    Create Login and Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <>
-     <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </>
   );
 }
