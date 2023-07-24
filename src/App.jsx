604f8ad2ecce613a7d878e11ec25c4306dd603e9
sumit saini <sumitsaini95488@gamil.com>
2023-07-24 13:20:15 +0530

    initials App  MARN STACK E-Commerce site

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold text-red-500">
+        this is MARN STACK E-COMMERCE SITE
+      </h1>
+    </>
+  );
+}
+
+export default App;
